54b00a0b9b21f97765a42ebf3a937a3259fb57cc
Chris Brown <cbrown@pivotal.io>
2015-06-04 11:47:08 -0700

    fix panic in test http handler
    * upgrading ginkgo revealed this
    * the non-202 test still ran through all the handlers which caused the
      assertions to fail with an index out of range error

--- datadog/datadog_test.go
+++ datadog/datadog_test.go
@@ -23,29 +23,6 @@ var _ = Describe("Datadog", func() {
 
 	BeforeEach(func() {
 		server = ghttp.NewServer()
-		server.AppendHandlers(ghttp.CombineHandlers(
-			ghttp.VerifyRequest("POST", "/api/v1/series", "api_key=api-key"),
-			func(w http.ResponseWriter, r *http.Request) {
-				var request request
-				Ω(json.NewDecoder(r.Body).Decode(&request)).Should(Succeed())
-				metric := request.Series[0]
-
-				Ω(metric.Name).Should(Equal("memory.limit"))
-				Ω(metric.Host).Should(Equal("web-0"))
-				Ω(metric.Tags).Should(ConsistOf("application:atc"))
-
-				Ω(metric.Points[0].Timestamp).Should(BeTemporally("~", time.Now(), time.Second))
-				Ω(metric.Points[0].Value).Should(BeNumerically("~", 4.52, 0.01))
-
-				Ω(metric.Points[1].Timestamp).Should(BeTemporally("~", time.Now(), time.Second))
-				Ω(metric.Points[1].Value).Should(BeNumerically("~", 23.22, 0.01))
-
-				Ω(metric.Points[2].Timestamp).Should(BeTemporally("~", time.Now(), time.Second))
-				Ω(metric.Points[2].Value).Should(BeNumerically("~", 23.25, 0.01))
-			},
-			ghttp.RespondWith(http.StatusAccepted, "{}"),
-		))
-
 		datadog.APIURL = server.URL()
 	})
 
@@ -55,23 +32,50 @@ var _ = Describe("Datadog", func() {
 		}
 	})
 
-	It("works", func() {
-		client := datadog.NewClient("api-key")
+	Context("when everything's great", func() {
+		BeforeEach(func() {
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.VerifyRequest("POST", "/api/v1/series", "api_key=api-key"),
+				func(w http.ResponseWriter, r *http.Request) {
+					var request request
+					Ω(json.NewDecoder(r.Body).Decode(&request)).Should(Succeed())
+					metric := request.Series[0]
 
-		err := client.PublishSeries(datadog.Series{
-			{
-				Name: "memory.limit",
-				Points: []datadog.Point{
-					{time.Now(), 4.52},
-					{time.Now(), 23.22},
-					{time.Now(), 23.25},
+					Ω(metric.Name).Should(Equal("memory.limit"))
+					Ω(metric.Host).Should(Equal("web-0"))
+					Ω(metric.Tags).Should(ConsistOf("application:atc"))
+
+					Ω(metric.Points[0].Timestamp).Should(BeTemporally("~", time.Now(), time.Second))
+					Ω(metric.Points[0].Value).Should(BeNumerically("~", 4.52, 0.01))
+
+					Ω(metric.Points[1].Timestamp).Should(BeTemporally("~", time.Now(), time.Second))
+					Ω(metric.Points[1].Value).Should(BeNumerically("~", 23.22, 0.01))
+
+					Ω(metric.Points[2].Timestamp).Should(BeTemporally("~", time.Now(), time.Second))
+					Ω(metric.Points[2].Value).Should(BeNumerically("~", 23.25, 0.01))
 				},
-				Host: "web-0",
-				Tags: []string{"application:atc"},
-			},
+				ghttp.RespondWith(http.StatusAccepted, "{}"),
+			))
 		})
 
-		Ω(err).ShouldNot(HaveOccurred())
+		It("works", func() {
+			client := datadog.NewClient("api-key")
+
+			err := client.PublishSeries(datadog.Series{
+				{
+					Name: "memory.limit",
+					Points: []datadog.Point{
+						{time.Now(), 4.52},
+						{time.Now(), 23.22},
+						{time.Now(), 23.25},
+					},
+					Host: "web-0",
+					Tags: []string{"application:atc"},
+				},
+			})
+
+			Ω(err).ShouldNot(HaveOccurred())
+		})
 	})
 
 	Context("when the server does not respond", func() {
