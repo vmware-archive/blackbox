dad210b8fd021dee435f5f9933c74558cd5c4417
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-08-30 16:17:43 +0200

    add source_dir array option

--- cmd/blackbox/main.go
+++ cmd/blackbox/main.go
@@ -42,6 +42,14 @@ func main() {
 		fileWatcher.Watch()
 	}()
 
+	for _, dir := range config.Syslog.SourceDirs {
+		go func(dir string) {
+			drainerFactory := syslog.NewDrainerFactory(config.Syslog.Destination, config.Hostname)
+			fileWatcher := blackbox.NewFileWatcher(logger, dir, group.Client(), drainerFactory)
+			fileWatcher.Watch()
+		}(dir)
+	}
+
 	err = <-running.Wait()
 	if err != nil {
 		logger.Fatalf("failed: %s", err)
