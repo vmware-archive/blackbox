af8faa19f630cd3b884c8df7b371b197a7f3141e
Michal Kuratczyk <mkuratczyk@pivotal.io>
2017-08-30 17:05:25 +0200

    one DrainerFactory should be enough

--- cmd/blackbox/main.go
+++ cmd/blackbox/main.go
@@ -40,9 +40,9 @@ func main() {
 	group := grouper.NewDynamic(nil, 0, 0)
 	running := ifrit.Invoke(sigmon.New(group))
 
+	drainerFactory := syslog.NewDrainerFactory(config.Syslog.Destination, config.Hostname)
 	if config.Syslog.SourceDir != "" {
 		go func() {
-			drainerFactory := syslog.NewDrainerFactory(config.Syslog.Destination, config.Hostname)
 			fileWatcher := blackbox.NewFileWatcher(logger, config.Syslog.SourceDir, group.Client(), drainerFactory)
 			fileWatcher.Watch()
 		}()
@@ -50,7 +50,6 @@ func main() {
 
 	for _, dir := range config.Syslog.SourceDirs {
 		go func(dir string) {
-			drainerFactory := syslog.NewDrainerFactory(config.Syslog.Destination, config.Hostname)
 			fileWatcher := blackbox.NewFileWatcher(logger, dir, group.Client(), drainerFactory)
 			fileWatcher.Watch()
 		}(dir)
